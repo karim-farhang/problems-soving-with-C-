9fc570d2d01ac2f6f5801cafd62cc0d138027f7e
karim-farhang <karim-farhang@users.noreply.github.com>
2025-01-27 11:00:22 +0430

    intial project

---
+++ main/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i= 10;
+    double d= 200.0;
+    char c = 'k';
+    string s = "Karim khan";
+    bool b= true;
+
+    cout<< i<<endl;
+    cout<<d<<endl;
+    cout<<c<<endl;
+    cout<<b<<endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
